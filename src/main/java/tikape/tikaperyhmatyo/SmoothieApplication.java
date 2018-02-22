5654b36ccbca8b8d2bcd0133ce7d24d8f4cbacad
nameisxi <tuomas.j.j.peltonen@gmail.com>
2018-02-22 16:12:12 +0200

    uusi näkym

--- src/main/java/tikape/tikaperyhmatyo/SmoothieApplication.java
+++ src/main/java/tikape/tikaperyhmatyo/SmoothieApplication.java
@@ -7,7 +7,9 @@ import java.util.Map;
 import spark.ModelAndView;
 import spark.Spark;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
+import tikape.tikaperyhmatyo.dao.IngredientDao;
 import tikape.tikaperyhmatyo.dao.SmoothieDao;
+import tikape.tikaperyhmatyo.dao.SmoothieIngredientDao;
 import tikape.tikaperyhmatyo.db.Database;
 import tikape.tikaperyhmatyo.domain.Smoothie;
 
@@ -15,20 +17,22 @@ public class SmoothieApplication {
     public static void main(String[] args) {
         Database db = new Database("jdbc:sqlite:smoothies.db");
         SmoothieDao sDao = new SmoothieDao(db);
+        IngredientDao iDao = new IngredientDao(db);
+        SmoothieIngredientDao siDao = new SmoothieIngredientDao(db);
         
         Spark.get("/", (req, res) -> {
             Map map = new HashMap<>();
             map.put("smoothies", sDao.findAll());
             
-            List<Smoothie> smoothies = sDao.findAll();
-            if (smoothies.isEmpty()) {
-                System.out.println("Ei toimi");
-            }
-            for (Smoothie smoothie : smoothies) {
-                System.out.println(smoothie.getName());
-            }
-            
             return new ModelAndView(map, "index");
+        }, new ThymeleafTemplateEngine());
+        
+        Spark.get("/id", (req, res) -> {
+            Map map = new HashMap<>();
+            map.put("ingredients", iDao.findAll());
+            map.put("smoothieIngredients", siDao.findAll());
+            
+            return new ModelAndView(map, "html tiedoston nimi tahan");
         }, new ThymeleafTemplateEngine());
     }
 }
